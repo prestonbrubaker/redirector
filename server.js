7709dda482eb13921e730cb38b25c3777bebd975
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2023-11-27 16:04:16 -0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 80; // Standard HTTP port
+
+app.get('*', (req, res) => {
+  res.redirect('http://73.101.217.220:58543' + req.url);
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
