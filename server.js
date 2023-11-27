dddb13ba73a365e7d7fb4edd4575d736c6c4ca41
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2023-11-27 16:17:32 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
+const http = require('http');
 const port = 80; // Standard HTTP port
 
-app.get('*', (req, res) => {
-  res.redirect('http://73.101.217.220:58543' + req.url);
+const targetPort = 58543; // The port you want to redirect to
+
+app.use((req, res) => {
+  const targetUrl = 'http://73.101.217.220:' + targetPort + req.url;
+  res.redirect(targetUrl);
 });
 
-app.listen(port, () => {
-  console.log(`Redirection server running on port ${port}`);
+const server = http.createServer(app);
+
+server.listen(port, () => {
+  console.log(`Redirect server running on port ${port}`);
 });
